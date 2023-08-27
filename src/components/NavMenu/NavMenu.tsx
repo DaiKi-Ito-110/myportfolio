9b1693d02a22d71a5e591db8df1c98c486947d32
daiki_ito <daiki_ito@nsketch.com>
2023-08-27 22:04:46 +0900

    move onClick in NavMenu

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 const NavMenu:React.FC<Props> = ({onClick}) => {
   return (
-    <nav onClick={onClick} className="pt-10 fixed w-screen h-screen bg-white  flex justify-center">
-      <div className='text-lg font-bold mt-10 w-10/12 h-1/3 flex flex-col justify-between items-center divide-y divide-dotted divide-gray-500'>
+    <nav  className="pt-10 fixed w-screen h-screen bg-white  flex justify-center">
+      <div onClick={onClick} className='text-lg font-bold mt-10 w-10/12 h-1/3 flex flex-col justify-between items-center divide-y divide-dotted divide-gray-500'>
         <NavElement linkTo='/works' elementName='WORKS'/>
         <NavElement linkTo='/about' elementName='ABOUT' />
         <NavElement linkTo='/contact' elementName='CONTACT' />
