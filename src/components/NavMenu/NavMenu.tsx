7b2184f127863ee6d30c5998d662481591a5d46d
daiki_ito <daiki_ito@nsketch.com>
2023-08-28 05:00:07 +0900

    add transiion to navmenu

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,4 +1,5 @@
 import NavElement from './NavElement';
+import { motion } from 'framer-motion';
 
 type Props = {
   onClick: ()=>void;
@@ -6,13 +7,18 @@ type Props = {
 
 const NavMenu:React.FC<Props> = ({onClick}) => {
   return (
-    <nav  className="z-50 pt-10 fixed w-screen h-screen bg-white  flex justify-center">
-      <div onClick={onClick} className='text-lg font-bold mt-10 w-10/12 h-1/3 flex flex-col justify-between items-center divide-y divide-dotted divide-gray-500'>
-        <NavElement linkTo='/works' elementName='WORKS'/>
-        <NavElement linkTo='/Profile' elementName='PROFILE' />
-        <NavElement linkTo='/contact' elementName='CONTACT' />
+    <motion.nav
+      initial={{y: '-2vh'}}
+      animate={{y: '0vh'}}
+      transition={{duration: 1}}
+      className="z-50 pt-10 fixed w-screen h-screen bg-white flex justify-center"
+      >
+        <div onClick={onClick} className='text-lg font-bold mt-10 w-10/12 h-1/3 flex flex-col justify-between items-center divide-y divide-dotted divide-gray-500'>
+          <NavElement linkTo='/works' elementName='WORKS'/>
+          <NavElement linkTo='/Profile' elementName='PROFILE' />
+          <NavElement linkTo='/contact' elementName='CONTACT' />
       </div>
-    </nav>
+    </motion.nav>
   )
 }
 
