81a3026f3bfd28ab96cd9562f65dd9e877c3befc
daiki_ito <daiki_ito@nsketch.com>
2023-08-30 20:27:19 +0900

    add waterDropletsSculputer images

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,13 @@ import Header from './components/Header.tsx'
 import Footer from './components/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode >
-    <BrowserRouter>
-      <Header />
-      <App />
-      <Footer />
-    </BrowserRouter>
+  <React.StrictMode>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <App />
+        <Footer />
+      </BrowserRouter>
+    </div>
   </React.StrictMode>,
 )
