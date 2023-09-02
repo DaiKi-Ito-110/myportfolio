04aeb9b90cad61012a34b1b2a8c559d6b01be359
daiki_ito <daiki_ito@nsketch.com>
2023-09-03 02:34:50 +0900

    create profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,34 @@
+import prof from '../profileInfo';
 
 const Profile:React.FC = () => {
   return (
-    <div>Profile</div>
+    <div className='w-5/6 sm:w-2/3 md:w-1/2 lg:w-2/5 flex flex-col items-center m-auto'>
+      <img src={prof.myImg} alt='image' />
+      <div className='w-full mt-10'>
+        <h1 className='text-right font-bold'>{prof.name}</h1>
+        <p className='whitespace-pre-wrap font-normal text-left mt-5 text-sm w-full break-words'>{prof.statement}</p>
+      </div>
+      <div className='w-full mt-16'>
+        <p className='text-left font-bold mb-5'>BIO</p>
+        {prof.bio.map((bio, i) => (
+          <p key={i} className='whitespace-pre-wrap text-xs mb-2'>{bio}</p>
+        )
+        )}
+      </div>
+      <div className='w-full mt-16'>
+        <p className='text-left font-bold mb-2'>SNS</p>
+        {prof.sns.map((sns, i) => (
+          <div key={i} className=''>
+            <a href={sns.link} className='underline underline-offset-2 hover:opacity-50 whitespace-pre-wrap text-xs'>{sns.title}</a>
+          </div>
+        )
+        )}
+      </div>
+      <div className='w-full mt-16'>
+        <p className='text-left font-bold mb-2'>CONTACT</p>
+        <p className='text-xs'>{prof.contact}</p>
+      </div>
+    </div>
   )
 }
 
---
+++ src/profileInfo.ts
@@ -0,0 +1,20 @@
+import thumbnail from '../src/assets/images/profile/thumbnail.webp'
+
+const profileInfo = {
+  myImg: thumbnail,
+  name: 'Daiki Ito | 伊藤大貴',
+  statement: '圧倒的な変数を持つ自然環境をうまく受け入れる関数となるような、システムや作品の制作を行っています。',
+  bio: [
+    '2019 - 2023\n早稲田大学 基幹理工学部 表現工学科',
+    '2023 - \n早稲田大学大学院 基幹理工学研究科 表現工学専攻'
+  ],
+  sns:[
+    {
+      title: 'Instagram',
+      link: 'https://www.instagram.com/daiki_ito_art/?igshid=OGQ5ZDc2ODk2ZA%3D%3D'
+    },
+  ],
+  contact: 'dai-29-itto@toki.waseda.jp'
+};
+
+export default profileInfo;
