c449bd92485f7492ac59aed982a92f2f58eeb373
daiki_ito <daiki_ito@nsketch.com>
2023-09-03 03:07:39 +0900

    create home

---
+++ src/components/HomeLinkToPage.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+  title: string;
+  image: string;
+  link: string;
+  isTextWhite?: boolean;
+}
+
+export const HomeLinkToPage:React.FC<Props> = ({title, image, isTextWhite, link}) => {
+  return (
+    <Link to={link} className="relative hover:opacity-90">
+      <p className={`${isTextWhite && 'z-50 text-gray-50'} absolute text-xl font-bold left-5 top-1/2 -translate-y-1/2`}>{title}</p>
+      <img className="" src={image} alt='image' />
+    </Link>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
+import { HomeLinkToPage } from "../components/HomeLinkToPage"
+
+//works
+import wThumbnail from '../../src/assets/images/works/waterDropletSculpture/thumbnail.webp'
+
+//profile
+import pThumbnail from '../../src/assets/images/profile/thumbnail.webp'
+
+//news
+import nThumbnail from '../../src/assets/images/news/thumbnail.webp'
 
 const Home:React.FC = () => {
   return (
-    <>
-      <div>Home</div>
-    </>
+    <div className="flex flex-col mt-10">
+      <HomeLinkToPage image={wThumbnail} link="/works" title="WORKS" isTextWhite={true} />
+      <HomeLinkToPage image={pThumbnail} link="/profile" title="PROFILE" isTextWhite={false} />
+      <HomeLinkToPage image={nThumbnail} link="/news" title="NEWS" isTextWhite={true} />
+    </div>
   )
 
 }
