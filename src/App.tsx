3ec462365ddbc5fb4c2136b6eb5440b3b26aceb9
daiki_ito <daiki_ito@nsketch.com>
2023-09-03 03:22:36 +0900

    move header footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,28 @@ import WorkDetail from './pages/WorkDetail'
 import { workInfo } from './workInfo'
 import { newsInfo } from './newsInfo'
 import NewsDetail from './components/NewsDetail'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App: React.FC = () => {
   return (
-      <Routes >
-        <Route path='/' element={<Home />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/works' element={<Works />} />
-        <Route path='/news' element={<News />} />
-        {workInfo.map((work, i) =>
-          <Route key={i} path={work.pageLink} element={<WorkDetail work={work} />} />
-        )}
-        {newsInfo.map((news, i) =>
-          <Route key={i} path={news.pageLink} element={<NewsDetail news={news} />} />
-        )}
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
+      <div>
+        <Header />
+        <Routes >
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/works' element={<Works />} />
+          <Route path='/news' element={<News />} />
+          {workInfo.map((work, i) =>
+            <Route key={i} path={work.pageLink} element={<WorkDetail work={work} />} />
+          )}
+          {newsInfo.map((news, i) =>
+            <Route key={i} path={news.pageLink} element={<NewsDetail news={news} />} />
+          )}
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,12 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 
 import App from './App.tsx'
-import Header from './components/Header.tsx'
-import Footer from './components/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <div className="text-gray-950 overflow-x-hidden">
-        <Header />
-        <App />
-        <Footer />
+      <App />
       </div>
     </BrowserRouter>
   </React.StrictMode>
