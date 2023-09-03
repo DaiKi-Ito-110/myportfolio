bf1732947bc1267e4c600c8adad7a5331cfaf6ff
daiki_ito <daiki_ito@nsketch.com>
2023-09-04 04:17:22 +0900

    change style works

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { Link} from "react-router-dom";
-import TransitionFromBottom from "./TransitionFromBottom.tsx";
-import TransitionScaleUpOnHover from "./TransitionScaleUpOnHover.tsx";
+import TransitionFromBottom from "./TransitionFromBottom";
+import TransitionScaleUpOnHover from "./TransitionScaleUpOnHover";
 
 type Props = {
   image: string;
@@ -12,14 +13,14 @@ type Props = {
 const Thumbnail:React.FC<Props> = ({image, title, delay, pageLink}) => {
   return (
     <TransitionFromBottom delay={delay}>
-        <div className="w-3/5 md:w-2/5 m-auto mt-10">
+        <div className="w-5/6 md:w-2/5 m-auto mt-10">
           <Link to={pageLink}>
             <div className="overflow-hidden">
               <TransitionScaleUpOnHover>
                 <img src={image} alt='thumbnail' />
               </TransitionScaleUpOnHover>
             </div>
-            <p className="mt-1 font-bold text-xs">{title}</p>
+            <p className="mt-1 font-bold text-sm">{title}</p>
           </Link>
         </div>
     </TransitionFromBottom>
