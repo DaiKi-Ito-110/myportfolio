f0fb5d0d5e6ed179ecc722a932e28076e85108a1
daiki_ito <daiki_ito@nsketch.com>
2023-09-04 04:34:21 +0900

    change position IGicon fotter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer:React.FC = () => {
         <FooterLink linkTo="/news" elementName="NEWS" />
       </div>
       <a href="https://www.instagram.com/daiki_ito_art/?igshid=OGQ5ZDc2ODk2ZA%3D%3D">
-        <BsInstagram className='text-xl absolute bottom-7 left-10' />
+        <BsInstagram className='text-xl absolute bottom-7 right-10' />
       </a>
     </div>
   )
