8ce28d8be5ace56442c7465ba1d6aebab08af623
daiki_ito <daiki_ito@nsketch.com>
2023-09-07 22:17:00 +0900

    scroll top

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 
 import App from './App.tsx'
+import ScrollTop from './components/ScrollTop'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollTop />
       <div className="text-gray-950 overflow-x-hidden">
       <App />
       </div>
