86d5bc5e85e09faae89096355209b7c8a613635f
daiki_ito <daiki_ito@nsketch.com>
2023-12-19 23:58:43 +0900

    change lg to md in ImageModal Icon

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -27,7 +27,7 @@ const ImageModal:React.FC<Props> = ({src,alt, className}) => {
       {isOpen &&
         <div onClick={handleClose} className='fixed top-0 left-0 w-screen h-screen bg-white bg-opacity-90 z-10'>
           <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full md:w-2/3' >
-            <AiOutlineCloseCircle className='absolute translate-y-full -bottom-5 right-1/2 translate-x-1/2 lg:-translate-y-full lg:-top-5 lg:right-5 text-2xl md:text-3xl hover:cursor-pointer'/>
+            <AiOutlineCloseCircle className='absolute translate-y-full -bottom-5 right-1/2 translate-x-1/2 md:-translate-y-full md:-top-5 md:right-5 text-2xl md:text-3xl hover:cursor-pointer'/>
             <ImageWithLoading src={src} alt={alt} className='m-auto'/>
           </div>
         </div>
